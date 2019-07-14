d784e2144c0c8a41245d73955c010db0d69844d1
Barak <bachillah@gmail.com>
2019-07-14 20:22:15 +0300

    second version of file

--- dummydata_conversion.py
+++ dummydata_conversion.py
@@ -2,8 +2,11 @@ import xlrd
 import pandas as pd
 
 excelfile=pd.ExcelFile("dummydata.xlsx")
-
-for i in range(1,11):
+i=1
+while i <11:
     dframe=excelfile.parse("Sheet"+str(i))
-    dframe.to_csv("Sheet"+str(i)+".csv",sep=",")
+    filename="Sheet"+str(i)+".csv"
+    dframe.to_csv(filename,sep=",")  #had  to change bc of unicode error in string function  UnicodeEncodeError: 'ascii' codec can't encode character u'\xe9' in position 3: ordinal not in range(128)
     dframe=None #TODO check if this line is really needed
+    print("done with "+str(i))
+    i+=1
