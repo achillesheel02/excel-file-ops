d64f4b608741e540f42d7f3c8388789c2c5b4525
Barak <bachillah@gmail.com>
2019-07-14 20:33:02 +0300

    second version of file

--- dummydata_conversion.py
+++ dummydata_conversion.py
@@ -6,7 +6,6 @@ i=1
 while i <11:
     dframe=excelfile.parse("Sheet"+str(i))
     filename="Sheet"+str(i)+".csv"
-    dframe.to_csv(filename,sep=",")  #had  to change bc of unicode error in string function  UnicodeEncodeError: 'ascii' codec can't encode character u'\xe9' in position 3: ordinal not in range(128)
+    dframe.to_csv(filename,sep=",",encoding='utf-8')  #had  to change bc of unicode error in string function  UnicodeEncodeError: 'ascii' codec can't encode character u'\xe9' in position 3: ordinal not in range(128)
     dframe=None #TODO check if this line is really needed
-    print("done with "+str(i))
     i+=1
